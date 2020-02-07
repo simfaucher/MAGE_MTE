6cde3be2bee54652d2907f09e62bb4b83d82c1d3
Fabien Monniot <fabien.monniot@altran.com>
2020-02-07 10:45:03 +0100

    Multi thread multi clients server/client

--- test_client.py
+++ test_client.py
@@ -1,62 +1,24 @@
 #!/usr/bin/env python3
 
-# import socket
-
-
-# TCP_IP = '127.0.0.1'
-# TCP_PORT = 5005
-# BUFFER_SIZE = 1024
-# MESSAGE = "Hello, World! :D ljfslkjgfldskgmlkfdmlgkmlkmlfkgmlfd fmldkjgmlfdkmlgkfdmlgkfkmldkgfml"
-
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# s.connect((TCP_IP, TCP_PORT))
-# s.send(MESSAGE.encode())
-# data = s.recv(BUFFER_SIZE)
-# s.close()
-
-# print("received data:", data)
-
-
-# Import socket module 
-import socket 
+import socket
+import sys
 import time
-  
-def Main(): 
-    # local host IP '127.0.0.1' 
-    host = '127.0.0.1'
-  
-    # Define the port on which you want to connect 
-    port = 12345
-  
-    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
-  
-    # connect to server on local computer 
-    s.connect((host,port)) 
-  
-    # message you send to server 
-    message = "shaurya says geeksforgeeks"
-    while True: 
-  
-        # message sent to server 
-        s.send(message.encode('ascii')) 
-  
-        # messaga received from server 
-        data = s.recv(1024) 
-  
-        # print the received message 
-        # here it would be a reverse of sent message 
-        print('Received from the server :',str(data.decode('ascii'))) 
+import cv2
+
+HOST, PORT = "localhost", 12345
+data = " ".join(sys.argv[1:])
+
+# Create a socket (SOCK_STREAM means a TCP socket)
+while True:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect((HOST, PORT))
+        # Connect to server and send data
+        sock.sendall(bytes(data + "\n", "utf-8"))
+
+        # Receive data from the server and shut down
+        received = str(sock.recv(1024), "utf-8")
+
+        print("Sent:     {}".format(data))
+        print("Received: {}".format(received))
 
         time.sleep(1)
-  
-        # # ask the client whether he wants to continue 
-        # ans = input('\nDo you want to continue(y/n) :') 
-        # if ans == 'y': 
-        #     continue
-        # else: 
-        #     break
-    # close the connection 
-    s.close() 
-  
-if __name__ == '__main__': 
-    Main() 
--- test_server.py
+++ test_server.py
@@ -1,88 +1,38 @@
 #!/usr/bin/env python3
 
-# import socket
+import socket
+import threading
+import socketserver
+import cv2
 
+class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
 
-# TCP_IP = '127.0.0.1'
-# TCP_PORT = 5005
-# BUFFER_SIZE = 20  # Normally 1024, but we want fast response
+    def handle(self):
+        data = str(self.request.recv(1024), 'ascii')
+        cur_thread = threading.current_thread()
+        response = bytes("{}: {}".format(cur_thread.name, data), 'ascii')
+        self.request.sendall(response)
 
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# s.bind((TCP_IP, TCP_PORT))
-# s.listen(1)
+class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
 
-# conn, addr = s.accept()
-# print('Connection address:', addr)
-# while 1:
-#     data = conn.recv(BUFFER_SIZE)
-#     if not data: break
-#     print("received data:", data)
-#     conn.send(data)  # echo
-# conn.close()
+if __name__ == "__main__":
+    # Port 0 means to select an arbitrary unused port
+    HOST, PORT = "localhost", 12345
 
+    server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
+    with server:
+        ip, port = server.server_address
 
-# import socket programming library 
-import socket 
-  
-# import thread module 
-from _thread import *
-import threading 
-  
-print_lock = threading.Lock() 
-  
-# thread function 
-def threaded(c): 
-    while True: 
-  
-        # data received from client 
-        data = c.recv(1024) 
-        if not data: 
-            print('Bye') 
-              
-            # lock released on exit 
-            print_lock.release() 
-            break
-  
-        # reverse the given string from client 
-        data = data[::-1] 
-  
-        # send back reversed string to client 
-        print(data)
-        c.send(b"Bien recu") 
-  
-    # connection closed 
-    c.close() 
-  
-  
-def Main(): 
-    host = '127.0.0.1'
-  
-    # reverse a port on your computer 
-    # in our case it is 12345 but it 
-    # can be anything 
-    port = 12345
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    s.bind((host, port)) 
-    print("socket binded to port", port) 
-  
-    # put the socket into listening mode 
-    s.listen(5) 
-    print("socket is listening") 
-  
-    # a forever loop until client wants to exit 
-    while True: 
-  
-        # establish connection with client 
-        c, addr = s.accept() 
-  
-        # lock acquired by client 
-        print_lock.acquire() 
-        print('Connected to :', addr[0], ':', addr[1]) 
-  
-        # Start a new thread and return its identifier 
-        start_new_thread(threaded, (c,)) 
-    s.close() 
-  
-  
-if __name__ == '__main__': 
-    Main() 
+        # Start a thread with the server -- that thread will then start one
+        # more thread for each request
+        server_thread = threading.Thread(target=server.serve_forever)
+        # Exit the server thread when the main thread terminates
+        server_thread.daemon = True
+        server_thread.start()
+        print("Server loop running in thread:", server_thread.name)
+
+        while True:
+            pass
+
+        server.shutdown()
