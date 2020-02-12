afe73277f4739f2ab5e484350df3c1cf8ec053be
Fabien Monniot <fabien.monniot@altran.com>
2020-02-12 16:37:00 +0100

    Tried to send images over sockets

--- test_client.py
+++ test_client.py
@@ -5,20 +5,27 @@ import sys
 import time
 import cv2
 
-HOST, PORT = "localhost", 12345
+image = cv2.imread("images/loup-640.jpg")
+image = cv2.resize(image, (320, 213))
+# cv2.imshow("Render", image)
+# cv2.waitKey(0)
+
+HOST, PORT = "localhost", 12330
 data = " ".join(sys.argv[1:])
 
 # Create a socket (SOCK_STREAM means a TCP socket)
-while True:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.connect((HOST, PORT))
-        # Connect to server and send data
-        sock.sendall(bytes(data + "\n", "utf-8"))
+# while True:
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.connect((HOST, PORT))
+    # Connect to server and send data
+    _, data = cv2.imencode('.jpg', image)
+    for i in range(0, int(data.shape[0] / 1024) + 1):
+        sock.sendall(data[i*1024: (i+1)*1024])
 
-        # Receive data from the server and shut down
-        received = str(sock.recv(1024), "utf-8")
+    # Receive data from the server and shut down
+    received = str(sock.recv(1024), "utf-8")
 
-        print("Sent:     {}".format(data))
-        print("Received: {}".format(received))
+    print("Sent:     {}".format(data))
+    print("Received: {}".format(received))
 
-        time.sleep(1)
+    # time.sleep(1)
--- test_server.py
+++ test_server.py
@@ -4,21 +4,38 @@ import socket
 import threading
 import socketserver
 import cv2
+import numpy as np
 
 class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
 
+    def setup(self):
+        print("setup")
+
     def handle(self):
-        data = str(self.request.recv(1024), 'ascii')
-        cur_thread = threading.current_thread()
-        response = bytes("{}: {}".format(cur_thread.name, data), 'ascii')
-        self.request.sendall(response)
+        # data = str(self.request.recv(1024), 'ascii')
+        # cur_thread = threading.current_thread()
+        # response = bytes("{}: {}".format(cur_thread.name, data), 'ascii')
+
+        data = self.request.recv(1024)
+        buff = np.fromstring(data, np.uint8)
+        buff = buff.reshape((1, -1))
+
+        image = cv2.imdecode(buff, cv2.IMREAD_COLOR)
+        cv2.imshow("Render", image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        
+        self.request.sendall(b"Bien recu")
+
+    def finish(self):
+        print("finish")
 
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     pass
 
 if __name__ == "__main__":
     # Port 0 means to select an arbitrary unused port
-    HOST, PORT = "localhost", 12345
+    HOST, PORT = "localhost", 12330
 
     server = ThreadedTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
     with server:
